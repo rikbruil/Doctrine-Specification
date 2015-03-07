fda2f7c35d9b424c6e294d36b2b2bd6605a8cc9e
Rik Bruil <rikbruil@users.noreply.github.com>
2015-03-08 00:04:30 +0100

    Written final tests

---
+++ spec/Query/LeftJoinSpec.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace spec\Rb\Doctrine\Specification\Query;
+
+use Doctrine\ORM\QueryBuilder;
+use Rb\Doctrine\Specification\Query\ModifierInterface;
+use PhpSpec\ObjectBehavior;
+
+class LeftJoinSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('user', 'authUser', 'a');
+    }
+
+    public function it_is_a_specification()
+    {
+        $this->shouldHaveType(ModifierInterface::class);
+    }
+
+    public function it_joins_with_default_dql_alias(QueryBuilder $queryBuilder)
+    {
+        $queryBuilder->leftJoin('a.user', 'authUser')->shouldBeCalled();
+        $this->modify($queryBuilder, 'a');
+    }
+
+    public function it_uses_local_alias_if_global_was_not_set(QueryBuilder $queryBuilder)
+    {
+        $this->beConstructedWith('user', 'authUser');
+        $queryBuilder->leftJoin('b.user', 'authUser')->shouldBeCalled();
+        $this->modify($queryBuilder, 'b');
+    }
+}
