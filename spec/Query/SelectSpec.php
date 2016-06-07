efeed940c5934042f21174027dca4ba7fd41267f
Kyle Tucker <kyleatucker@gmail.com>
2016-06-07 15:26:58 -0400

    update spec

--- spec/Query/SelectSpec.php
+++ spec/Query/SelectSpec.php
@@ -4,6 +4,8 @@ namespace spec\Rb\Specification\Doctrine\Query;
 
 use Doctrine\ORM\QueryBuilder;
 use PhpSpec\ObjectBehavior;
+use Rb\Specification\Doctrine\Exception\InvalidArgumentException;
+use Rb\Specification\Doctrine\Query\Select;
 
 class SelectSpec extends ObjectBehavior
 {
@@ -11,11 +13,36 @@ class SelectSpec extends ObjectBehavior
     {
         $alias  = 'a';
         $entity = 'foo';
-        $this->beConstructedWith($entity);
+        $type = Select::ADD_SELECT;
+        $this->beConstructedWith($entity, $type);
 
         $queryBuilder->addSelect($entity)->shouldBeCalled();
 
         $this->isSatisfiedBy('foo')->shouldReturn(true);
         $this->modify($queryBuilder, $alias);
+    }
+
+    public function it_should_replace_selects_in_query_builder(QueryBuilder $queryBuilder)
+    {
+        $alias  = 'a';
+        $entity = 'foo';
+        $type = Select::SELECT;
+        $this->beConstructedWith($entity, $type);
+
+        $queryBuilder->select($entity)->shouldBeCalled();
+
+        $this->isSatisfiedBy('foo')->shouldReturn(true);
+        $this->modify($queryBuilder, $alias);
+    }
+
+    public function it_throws_an_exception_when_setting_illegal_type()
+    {
+        $entity = 'foo';
+        $this->beConstructedWith($entity);
+        
+        $this->setType(Select::ADD_SELECT);
+
+        $this->shouldThrow(InvalidArgumentException::class)
+            ->during('setType', ['foo']);
     }
 }
